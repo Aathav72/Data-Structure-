2bbe21288468170ea2f4ff2fb9fc56833d1f4812
Aathavan G <162794403+Aathav72@users.noreply.github.com>
2025-07-09 15:23:07 +0530

    Add files via upload
    Signed-off-by: Aathavan G  <162794403+Aathav72@users.noreply.github.com>

---
+++ Basic String operation.py
@@ -0,0 +1,25 @@
+
+brand = "lenovo"
+length = len(brand)
+print("Length of the string: ", length)
+
+
+brand1 = "Dell"
+brand2 = "HP"
+combined = brand1 + " " + brand2
+print("Combined String:", combined)
+
+
+brand3 = "ASUS"
+second_char = brand3[1]  # Index 1 is the second character
+print("The second character is", second_char)
+
+
+brand4 = "Apple"
+for char in brand4:
+    print(char)
+
+
+brand5 = "Acer"
+uppercase_brand = brand5.upper()
+print("Uppercase string:", uppercase_brand)
